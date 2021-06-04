6a0e87f84cf2e68ab467b4fc0763973791b3cf21
unknown <you@example.com>
2021-06-04 14:36:33 +0300

    used an arraylist, which doesn't have a type(not inside generics), elemts has to be casted(boxed) to be accessed

--- VeryFirst/Program.cs
+++ VeryFirst/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace VeryFirst
 {
@@ -9,31 +10,39 @@ namespace VeryFirst
         {
             //Console.WriteLine("Hello World!");
 
-            List<String> customers = new List<string>();
-            customers.Add("Heineken");
-            customers.Add("Marriot Executives");
-            customers.Add("Starbucks");
-            customers.Add("McDonald's");
+            //List<String> customers = new List<string>();
+            //customers.Add("Heineken");
+            //customers.Add("Marriot Executives");
+            //customers.Add("Starbucks");
+            //customers.Add("McDonald's");
 
-            foreach (var item in customers)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in customers)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
-            Console.WriteLine(customers.Count);
-            Console.WriteLine(customers[3]);
+            //Console.WriteLine(customers.Count);
+            //Console.WriteLine(customers[3]);
 
-            Dictionary<String, String> config = new Dictionary<string, string>();
-            config.Add("resolution", "1920x1080");
-            config.Add("title", "Some_website");
 
-            Console.WriteLine(config["title"]);
+            //Dictionary<String, String> config = new Dictionary<string, string>();
+            //config.Add("resolution", "1920x1080");
+            //config.Add("title", "Some_website");
 
-            foreach (var item in config)
-            {
-                Console.WriteLine(item.Key);
-                Console.WriteLine(item.Value);
-            }
+            //Console.WriteLine(config["title"]);
+
+            //foreach (var item in config)
+            //{
+            //    Console.WriteLine(item.Key);
+            //    Console.WriteLine(item.Value);
+            //}
+
+
+            ArrayList list = new ArrayList();
+            list.Add("a string");
+
+      
+            Console.WriteLine(list[0].ToString().Length);
 
         }
     }
