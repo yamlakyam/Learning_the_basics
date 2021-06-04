e9a62e8c582bec7558826d8c4b6b0eeebab27c43
unknown <you@example.com>
2021-06-04 14:27:35 +0300

    used a dictionary

--- VeryFirst/Program.cs
+++ VeryFirst/Program.cs
@@ -7,7 +7,7 @@ namespace VeryFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
             List<String> customers = new List<string>();
             customers.Add("Heineken");
@@ -20,6 +20,20 @@ namespace VeryFirst
                 Console.WriteLine(item);
             }
 
+            Console.WriteLine(customers.Count);
+            Console.WriteLine(customers[3]);
+
+            Dictionary<String, String> config = new Dictionary<string, string>();
+            config.Add("resolution", "1920x1080");
+            config.Add("title", "Some_website");
+
+            Console.WriteLine(config["title"]);
+
+            foreach (var item in config)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
+            }
 
         }
     }
